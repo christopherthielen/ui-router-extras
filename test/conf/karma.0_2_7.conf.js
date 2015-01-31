c6b41a9e920f1ea0d5d6686ed78f39323066502a
christopherthielen <github@sandgnat.com>
2015-01-31 16:54:51 -0600

    chore(extras): removed karma configs, which are no longer needed

--- test/conf/karma.0_2_7.conf.js
+++
@@ -1,7 +0,0 @@
-var allFiles = require('../../files.js');
-var baseConfig = require("./karma.base.conf.js");
-var _ = require("lodash");
-
-var fullConfig = _.clone(baseConfig, true);
-fullConfig.files = fullConfig.files.concat(allFiles.files.ui_router_0_2_7).concat(allFiles.minfiles.src);
-module.exports = function(config) { config.set(fullConfig); };
--- test/conf/karma.dev.conf.js
+++
@@ -1,5 +0,0 @@
-var allFiles = require('../../files.js');
-var standardConf = require("./karma.base.conf");
-standardConf.files = standardConf.files.concat(allFiles.files.ui_router_0_2_10).concat(allFiles.files.src);
-module.exports = function(config) { config.set(standardConf); };
-
