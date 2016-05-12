e99f39324f942810192b5c627c20f6c54b6cd03f
Angel Ortiz <aandresortiz@gmail.com>
2016-05-12 01:24:01 -0300

    Update stickydemo.js

--- example/sticky/stickydemo.js
+++ example/sticky/stickydemo.js
@@ -1,35 +1,119 @@
 (function() {
   "use strict";
   var app = angular.module("ct.ui.router.extras.examples.sticky",
-    [ 'ct.ui.router.extras.sticky', 'ct.ui.router.extras.dsr', 'ct.ui.router.extras.examples', 'ct.ui.router.extras.statevis' ]);
+    [ 'ct.ui.router.extras.sticky',
+      'ct.ui.router.extras.dsr',
+      'ct.ui.router.extras.examples',
+      'ct.ui.router.extras.statevis'
+    ]);
   
   app.config(function ($stateProvider, $stickyStateProvider, $urlRouterProvider) {
     $stickyStateProvider.enableDebug(true);
     var states = [];
-    states.push({ name: 'aside1',                     url: '/aside1',       controller: 'timerCtrl',  templateUrl: '../partials/aside.html' });
-    states.push({ name: 'aside2',                     url: '/aside2',       controller: 'timerCtrl',  templateUrl: '../partials/aside.html' });
-    states.push({ name: 'top',                        url: '/',             
-                  views: {
-                    'instructions': { controller: 'timerCtrl', templateUrl: 'instructions.html' },
-                    '@':             { controller: 'tabCtrl', templateUrl: 'top.html' }
-                  }});
+    states.push({ 
+      name: 'aside1',
+      url: '/aside1',
+      controller: 'timerCtrl',
+      templateUrl: '../partials/aside.html'
+    });
+    states.push({ 
+      name: 'aside2',
+      url: '/aside2',
+      controller: 'timerCtrl',
+      templateUrl: '../partials/aside.html'
+    });
+    states.push({
+      name: 'top',
+      url: '/',             
+      views: {
+        'instructions': { 
+          controller: 'timerCtrl',
+          templateUrl: 'instructions.html'
+        },
+        '@': { 
+          controller: 'tabCtrl',
+          templateUrl: 'top.html'
+        }
+      }
+    });
     
     // Personnel tab
-    states.push({ name: 'top.people',                 url: 'people',        
-                  views: { 'peopletab': { controller: 'peopleCtrl', templateUrl: '../partials/people.html'} },
-                  resolve: { foo: function() { console.log("resolving 'foo' for top.people (issue #22)"); return "foo"; }},
-                  deepStateRedirect: { default: "top.people.managerlist" }, sticky: true });
-    states.push({ name: 'top.people.managerlist',     url: '/managers',     controller: 'managerCtrl',templateUrl: '../partials/managers.html' });
-    states.push({ name: 'top.people.manager',         url: '/manager/:mid', controller: 'managerCtrl',templateUrl: '../partials/manager.html' });
-    states.push({ name: 'top.people.manager.emplist', url: '/emps',         controller: 'empCtrl',    templateUrl: '../partials/emps.html' });
-    states.push({ name: 'top.people.manager.emp',     url: '/emp/:eid',     controller: 'empCtrl',    templateUrl: '../partials/emp.html' });
+    states.push({
+      name: 'top.people',
+      url: 'people',        
+      views: {
+        'peopletab': {
+          controller: 'peopleCtrl',
+          templateUrl: '../partials/people.html'}
+      },
+      resolve: {
+        foo: function() {
+          console.log("resolving 'foo' for top.people (issue #22)");
+          return "foo";
+        }
+      },
+      deepStateRedirect: { default: "top.people.managerlist" },
+      sticky: true
+    });
+    
+    states.push({
+      name: 'top.people.managerlist',
+      url: '/managers',
+      controller: 'managerCtrl',
+      templateUrl: '../partials/managers.html'
+    });
+    
+    states.push({
+      name: 'top.people.manager',
+      url: '/manager/:mid',
+      controller: 'managerCtrl',
+      templateUrl: '../partials/manager.html'
+    });
+    
+    states.push({
+      name: 'top.people.manager.emplist',
+      url: '/emps',
+      controller: 'empCtrl',
+      templateUrl: '../partials/emps.html'
+    });
+    
+    states.push({
+      name: 'top.people.manager.emp',
+      url: '/emp/:eid',
+      controller: 'empCtrl',
+      templateUrl: '../partials/emp.html'
+    });
 
     // Inventory tab
-    states.push({ name: 'top.inv',                    url: 'inv',           
-                  views: { 'invtab': { controller: 'invCtrl',    templateUrl: '../partials/inv.html' } },
-                  deepStateRedirect: { default: "top.inv.storelist" }, sticky: true });
-    states.push({ name: 'top.inv.storelist',          url: '/stores',       controller: 'storeCtrl',  templateUrl: '../partials/stores.html' });
-    states.push({ name: 'top.inv.store',              url: '/store/:sid',   controller: 'storeCtrl',  templateUrl: '../partials/store.html' });
+    states.push({
+      name: 'top.inv',
+      url: 'inv',
+      views: {
+        'invtab': {
+          controller: 'invCtrl',
+          templateUrl: '../partials/inv.html'
+        }
+      },
+      deepStateRedirect: {
+        default: "top.inv.storelist"
+      },
+      sticky: true
+    });
+    
+    states.push({
+      name: 'top.inv.storelist',
+      url: '/stores',
+      controller: 'storeCtrl',
+      templateUrl: '../partials/stores.html'
+    });
+    
+    states.push({
+      name: 'top.inv.store',
+      url: '/store/:sid',
+      controller: 'storeCtrl',
+      templateUrl: '../partials/store.html'
+    });
+    
     states.push({ name: 'top.inv.store.productlist',  url: '/products',     controller: 'productCtrl',templateUrl: '../partials/products.html' });
     states.push({ name: 'top.inv.store.product',      url: '/product/:pid', controller: 'productCtrl',templateUrl: '../partials/product.html' });
 
